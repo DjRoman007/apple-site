e76c95fc866e9aa9ef14873d27c8662ccb21d8e2
DjRoman007 <hhoollmmees@gmail.com>
2019-09-11 13:56:00 +0000

    apple site

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-let message = 'Welcome to Your apple!';
-alert(message);
+'use strict'
+let welcome = 'Welcome to Your apple!';
+alert(welcome);
 //clock
 const clockElement = document.querySelector('.clock');
 setInterval( () => clockElement.innerText = (new Date()).toLocaleTimeString(), 1000 );
